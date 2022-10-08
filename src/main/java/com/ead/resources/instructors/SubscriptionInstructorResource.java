28b20279ced6afce4a4fd4fe639665753e3ce52b
luisevoluum <luis.oliveira@evoluum.com.br>
2022-10-08 02:34:42 -0300

    feat: ajuste auth user

--- src/main/java/com/ead/resources/instructors/SubscriptionInstructorResource.java
+++ src/main/java/com/ead/resources/instructors/SubscriptionInstructorResource.java
@@ -6,6 +6,7 @@ import com.ead.services.instructors.SubscriptionInstructorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class SubscriptionInstructorResource {
 
     private final SubscriptionInstructorService service;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/instructors/subscription")
     public ResponseEntity<InstructorResponse> call(@RequestBody @Valid InstructorRequest request) {
         log.info("POST SubscriptionInstructorResource.call Request: {}", request);
--- src/main/java/com/ead/services/instructors/SubscriptionInstructorService.java
+++ src/main/java/com/ead/services/instructors/SubscriptionInstructorService.java
@@ -2,13 +2,16 @@ package com.ead.services.instructors;
 
 import com.ead.assembler.InstructorAssembler;
 import com.ead.assembler.UserModelAssembler;
+import com.ead.enums.AuthorityTypeE;
 import com.ead.enums.UserTypeE;
+import com.ead.model.AuthorityModel;
 import com.ead.model.UserModel;
 import com.ead.model.request.instructors.InstructorRequest;
 import com.ead.model.response.instructors.InstructorResponse;
 import com.ead.repositories.UserRepository;
 import com.ead.services.UpdateUserAndPublisherService;
 import com.ead.services.UserByIdOrElseThrowService;
+import com.ead.services.authorities.AuthorityByNameOrElseThrowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -25,8 +28,12 @@ public class SubscriptionInstructorService {
 
     private final UpdateUserAndPublisherService updateUserAndPublisherService;
 
+    private final AuthorityByNameOrElseThrowService authorityByNameOrElseThrowService;
+
     public InstructorResponse call(final InstructorRequest request) {
         log.debug("SubscriptionInstructorService.call Request: {}", request);
+
+        final AuthorityModel authority = this.authorityByNameOrElseThrowService.call(AuthorityTypeE.ROLE_INSTRUCTOR);
 
         final UserModel user = this.userByIdOrElseThrowService.call(request.getUserId());
 
@@ -34,6 +41,7 @@ public class SubscriptionInstructorService {
 
         user.setTypeE(UserTypeE.INSTRUCTOR);
         user.setUpdatedAt(OffsetDateTime.now());
+        user.getListAuthority().add(authority);
 
         final UserModel userSaved = this.updateUserAndPublisherService.call(user);
 
